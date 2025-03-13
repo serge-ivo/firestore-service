8ca580c8ec8ab14055574e8f6380126dda6fceb2
kovarny <sivochkin@gmail.com>
2025-03-13 23:41:49 +1100

    fix

--- dist/FirestoreDataConverter.d.ts
+++
@@ -1,3 +0,0 @@
-import { FirestoreDataConverter } from 'firebase/firestore';
-declare const createFirestoreDataConverter: <T>() => FirestoreDataConverter<T>;
-export default createFirestoreDataConverter;
--- dist/index.d.ts
+++
@@ -1,2 +0,0 @@
-export { default as FirestoreService } from "./firestoreService";
-export * from "./firestoreService";
